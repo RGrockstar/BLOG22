525523c172635e0708b71c377745f217d53ba156
RGrockstar <74416609+RGrockstar@users.noreply.github.com>
2021-01-18 15:16:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var inputs = [];
+function paragraph1(){
+for(var i=1; i<=6; i++)
+{
+    inputs.push(document.getElementById("para1_input_box_"+ i).value);
+}
+inputs.join(". ");
+document.getElementById("get_paragraph1").innerHTML=inputs.join(". ");
+}
